9377f29ddd41b2ad1c248db2c0c885496230b92a
lenildojunior <lenildo.ze@gmail.com>
2020-04-14 21:12:13 +0000

    adicionando o cadastro de dispositivos e localizacoes no mapa

--- flask_app.py
+++ flask_app.py
@@ -2,7 +2,7 @@ import functools
 import folium
 from flask import Flask, render_template , redirect, request,url_for,flash,g,session
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import text
+from sqlalchemy import text #conversao dos parametos de uma string para a query
 from draw_graph import build_graph
 
 
@@ -15,9 +15,9 @@ app.add_url_rule('/', endpoint='index') #Comando para instanciar a pagina '/' co
 
 SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
     username = "lenildojunior",
-    password="senha ddo mysql",
+    password="mysqlAdmin",
     hostname="lenildojunior.mysql.pythonanywhere-services.com",
-    databasename="nome do banco",
+    databasename="lenildojunior$frutas",
 )
 
 #Set the app config values for Database connection
@@ -25,7 +25,6 @@ app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
 app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
-
 
 class frutas(db.Model):
     __tablename__ = "frutas"
@@ -37,6 +36,20 @@ class usuario(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(4096))
     password = db.Column(db.String(4096))
+
+class dispositivos(db.Model):
+    __tablename__ = "dispositivos"
+    id = db.Column(db.String(4096), primary_key=True)
+    marca_modelo = db.Column(db.String(4096))
+    data_cadastro = db.Column(db.DateTime)
+    criado_por = db.Column(db.String(4096))
+    ativo = db.Column(db.Integer)
+
+class localizacao(db.Model):
+    __tablename__ = "localizacao"
+    id_dispositivo = db.Column(db.String(4096), primary_key=True)
+    latitude = db.Column(db.String(4096))
+    longitude = db.Column(db.String(4096))
 
 
 
@@ -55,12 +68,36 @@ def login_required(view):
 @login_required #Indica que para acessar esta pagina, precisa estar logado
 def index():
     if request.method =="GET":
-        fruits = frutas.query.all()
-        return render_template("dashboard.html", fruits=fruits)
+        lista_dispositivos = dispositivos.query.all()
+        return render_template("dashboard.html", dispositivos=lista_dispositivos)
     return redirect('login')
 
 
+@app.route('/cadastro_de_dispositivos',methods=('GET', 'POST'))
+@login_required
+def cadastro_de_dispositivos():
+    if request.method == 'POST':
+        id_form = request.form['IMEI']
+        marca_modelo = request.form['marca_modelo']
+        error = None
+        select_text = text("SELECT id from dispositivos where id= :id_form")
+        if not id_form:
+            error = 'IMEI is required.'
+        elif not marca_modelo:
+            error = 'Marca/modelo is required.'
+        elif db.engine.execute(select_text, id_form = id_form
+        ).fetchone() is not None:
+            error = 'Device {} is already registered.'.format(id_form)
+
+        if error is None:
+            insert_query = text("INSERT INTO dispositivos (id, marca_modelo, data_cadastro, criado_por, ativo) VALUES (:id_form, :marca_modelo, NOW(), :username, 1)")
+            db.engine.execute(insert_query, id_form = id_form, marca_modelo = marca_modelo , username = g.user['username'])
+            return redirect('/')
+        flash(error)
+    return render_template('cadastro_de_dispositivos.html')
+
 @app.route('/register', methods=('GET', 'POST'))
+@login_required
 def register():
     if request.method == 'POST':
         username = request.form['username']
@@ -135,10 +172,12 @@ def graphs():
 @app.route('/mapa')
 @login_required
 def mapa():
+    lista_coordenadas = localizacao.query.all()
     start_coords = (-5.834575, -35.2207787) #Coordenadas de Natal
     folium_map = folium.Map(location=start_coords, zoom_start=14)
     site = url_for('graphs')
     html = "<a href= '" + site + "' target='_blank'>Ver gráficos</h1></a>"
-    folium.Marker(location = (-5.8112895,-35.2084236),popup=folium.Popup(html), icon=folium.Icon(color='green')).add_to(folium_map)#Adicionando uma marcação no mapa
-    return folium_map._repr_html_()
-
+    for coordenadas in lista_coordenadas:
+        folium.Marker(location = (float((coordenadas.latitude).replace(',','.')),float((coordenadas.longitude).replace(',','.'))),popup=folium.Popup(html), icon=folium.Icon(color='green')).add_to(folium_map)#Adicionando uma marcação no mapa
+    folium_map.save('mysite/templates/folium.html')
+    return render_template('mapa.html')
